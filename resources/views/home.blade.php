2210aabd44d6b68a53d70586a423eb31b18885fa
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2022-10-13 16:16:34 +0545

    salmani

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,8 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
+                
+
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
